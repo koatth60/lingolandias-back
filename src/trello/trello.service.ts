068dc1c340397f8f8753ff6df494ae7413bc6694
Koatth <cm33673@gmail.com>
2026-02-10 21:51:58 -0500

    Trello Integration

--- src/trello/trello.service.ts
+++ src/trello/trello.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TrelloService {
-  // Almacenamiento en memoria
+  
   private tokenStore = new Map<string, string>();
 
   getOAuthUrl(email: string): string {
